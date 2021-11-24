26aef16c8e26b37c0ac7c847ee9bd684d2dd99fb
nursejason <nursejason@github.com>
2021-11-24 10:28:44 -0700

    Goreleaser is weird

--- upload.sh
+++ upload.sh
@@ -1,4 +1,4 @@
 for filename in $(find . -name "*.msi" -o -name "*.rpm" -o -name "*.deb" -o -name "*.zip" -o -name "*.tar.gz");do
-	echo "===> Uploading to GH test: ${filename}"
-	gh release upload "test" "${filename}" --clobber
+	echo "===> Uploading to GH $TAG: ${filename}"
+	gh release upload "${TAG}" "${filename}" --clobber
 done
